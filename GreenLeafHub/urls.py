60dc8370266bd6cfb6929a0062716b3757c82bda
arniishmahendar <arniish007@gmail.com>
2024-07-20 22:27:31 -0400

    revert

--- GreenLeafHub/urls.py
+++ GreenLeafHub/urls.py
@@ -19,16 +19,13 @@ from django.urls import path
 from django.conf.urls import include
 from django.contrib.auth import views as auth_views
 
-from GreenWebsite.views import (dashboard, logout_user, signup_view, login_user, CustomPasswordResetView, send_test_email,product,submit_review,add_to_cart,view_cart,add_product ,product_list,edit_product, payment_view, process_payment ,
-                                search,
-                                order_history,
-                                product_gallery)
+from GreenWebsite.views import dashboard, logout_user, signup_view, login_user, CustomPasswordResetView, send_test_email
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dashboard, name='home'),
     path('dashboard/', dashboard, name='dashboard'),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('signup/', signup_view, name='signup'),
     path('password_reset/',CustomPasswordResetView.as_view(),name='password_reset'),
@@ -36,19 +33,4 @@ urlpatterns = [
     path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),name='password_reset_confirm'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     path('send_mail/',send_test_email,name='send_mail'),
-    path('product/<int:pk>',product,name='product'),
-    path('search/', search, name='search'),
-    # path('product_detail/<int:pk>',product,name='product'),
-    path('product-gallery/',product_gallery,name='product_gallery'),
-
-    path('submit_review/<int:product_id>/',submit_review,name='submit_review'),
-    path('add_product', add_product, name='add_product'),
-    path('product_list', product_list, name='product_list'),
-    path('edit_product/<int:pk>', edit_product, name='edit_product'),
-    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    path('cart/', view_cart, name='cart'),
-    path('payment/', payment_view, name='payment_page'),
-    path('process_payment/', process_payment, name='process_payment'),
-    path('order_history/', order_history, name='order_history'),
-    path('payment/', payment_view, name='payment_page'),
 ]
