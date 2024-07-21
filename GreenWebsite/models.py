14c357718b131dc55e7dc4b0d066efe1870ac771
arniishmahendar <arniish007@gmail.com>
2024-07-20 22:50:04 -0400

    order-history update

--- GreenWebsite/models.py
+++ GreenWebsite/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     on_sale = models.BooleanField(default=False)
-    image = models.ImageField(default='default.png',blank=True, null=True,upload_to='product_img/')
+    image_url = models.URLField()
     description = models.TextField()
     review_count = models.IntegerField(default=0)
     rating = models.FloatField(default=0.0)
@@ -53,3 +53,15 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, default='Pending')
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
