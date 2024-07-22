eccbe30b4ef43b17570921e60479384550add246
jo-EL <31573942+joelmatt@users.noreply.github.com>
2024-07-22 13:29:45 -0400

    Merge conflict missed out changes added back
    crispy form missed out from the settings.py in previous merge is added again thorugh this push

--- GreenLeafHub/settings.py
+++ GreenLeafHub/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'GreenWebsite.apps.GreenwebsiteConfig',
-   
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -133,5 +134,6 @@ LOGIN_URL = '/login/'
 #LOGIN_REDIRECT_URL='/'
 #LOGOUT_REDIRECT_URL='/logout'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
