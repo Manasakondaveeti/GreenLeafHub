fa2e029b18ea3204eabac805a156cd5284f0d239
Manasa Kondaveeti <kondaveetimanasa77@gmail.com>
2024-07-23 01:06:49 -0400

    solving issues with design

--- GreenWebsite/forms.py
+++ GreenWebsite/forms.py
@@ -25,7 +25,8 @@ class UserProfileForm(forms.ModelForm):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100,widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email or Username'}))
+    username = forms.CharField(label='Username', max_length=100,widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                                              'placeholder': 'Email or Username'  ,  'autocapitalize': 'none'}))
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
